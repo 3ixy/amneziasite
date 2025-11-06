84c9055c47590918b456999a04c4c90a48dad4f2
3ixy <106113486+3ixy@users.noreply.github.com>
2025-11-06 21:26:55 +0300

    initial

---
+++ amnezia-docs/src/components/HomepageFeatures/index.js
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+ 
+];
+function Feature({Svg, title, description}) {
+  return (
+ ''
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+''
+  );
+}
