3417c81fad8cbb399a0b19db7e54fed4d361956e
3ixy <106113486+3ixy@users.noreply.github.com>
2025-11-08 00:28:38 +0300

    upload

--- docs/docs/parse.py
+++ docs/docs/parse.py
@@ -1,21 +1,10 @@
 import os
-import html2text
 
-converter = html2text.HTML2Text()
-converter.ignore_links = False
-for root, _, files in os.walk(".."):
+for root, _, files in os.walk("."):
     for f in files:
         if f.endswith(".md"):
-            md_path = os.path.join(root, f)
-            with open(md_path, "r", encoding="utf-8") as fin:
-                lines = fin.readlines()
-
-            if len(lines) > 86:
-                lines = lines[86:]  # пропустить первые 86 строк
-            else:
-                lines = []  # если меньше 86 строк — удалить всё
-
-            with open(md_path, "w", encoding="utf-8") as fout:
-                fout.writelines(lines)
-
-            print(f"🧹 Trimmed first 86 lines in: {md_path}")
+            path = os.path.join(root, f)
+            with open(path, "rb") as fin:
+                data = fin.read()
+            if b"\x00" in data:
+                print("⚠️ Нулевой байт найден в:", path)
